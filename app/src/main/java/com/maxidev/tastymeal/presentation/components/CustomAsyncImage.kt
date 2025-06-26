02d072d5626dd40cf76809043d45434d5778c19c
Maxi Pelizzoni <maxi96p@gmail.com>
2025-06-26 17:49:40 -0300

    Shimmer effect and string resources added.

--- app/src/main/java/com/maxidev/tastymeal/presentation/components/CustomAsyncImage.kt
+++ app/src/main/java/com/maxidev/tastymeal/presentation/components/CustomAsyncImage.kt
@@ -1,12 +1,20 @@
 package com.maxidev.tastymeal.presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import coil3.compose.AsyncImage
+import coil3.compose.SubcomposeAsyncImage
+import com.valentinilk.shimmer.ShimmerBounds
+import com.valentinilk.shimmer.rememberShimmer
+import com.valentinilk.shimmer.shimmer
 
 @Composable
 fun CustomAsyncImage(
@@ -18,13 +26,31 @@ fun CustomAsyncImage(
     height: Dp = Dp.Unspecified,
     onClick: () -> Unit = {}
 ) {
-    // TODO: Add shimmer or placeholder
-    AsyncImage(
+    SubcomposeAsyncImage(
         model = model,
         contentDescription = contentDescription,
         contentScale = contentScale,
+        loading = { ShimmerImageLoading() },
         modifier = modifier
             .height(height)
             .clickable { onClick() }
     )
+}
+
+@Composable
+private fun ShimmerImageLoading() {
+    val shimmerInstance = rememberShimmer(shimmerBounds = ShimmerBounds.View)
+    
+    Box(
+        modifier = Modifier
+            .shimmer(customShimmer = shimmerInstance)
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.onBackground)
+    )
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun ShimmerImageLoafingPreview() {
+    ShimmerImageLoading()
 }
